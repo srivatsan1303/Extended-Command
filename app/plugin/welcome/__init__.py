a93e0f57eb7867d041f0653aa78381bf86ce9f59
srivatsan1303 <sj796@njit.edu>
2024-03-09 23:45:29 -0500

    adding logs to the commands present in plugins

--- app/plugin/welcome/__init__.py
+++ app/plugin/welcome/__init__.py
@@ -1,6 +1,8 @@
 from app.commands import Command
+import logging
 
 
 class WelcomeCommand(Command):
     def execute(self):
+        logging.info("Welcome! Have a nice day")
         print("Welcome! Have a nice day")
